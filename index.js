7a773f168b2a7077abb21ff97c8fad6ddcaafc19
Angel Reyes <a22110083@ceti.mx>
2024-02-12 20:15:20 -0600

    Actividad 1 - Desarrollo Web 2 - Finalizada

--- index.js
+++ index.js
@@ -1,43 +1,67 @@
 //Angel Emanuel Mendoza Reyes
 //22110083
-//Grupo: 5 p
-//Fecha: 12/2/2024
+//Grupo: 5 P
+//Fecha: 12/02/2024
 const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.send("Mascotas");
-});
-
 let pets = [
-  { id: 1, name: "Dog" },
-  { id: 2, name: "Cat" },
+  { id: 1, name: "dog" },
+  { id: 2, name: "cat" },
+  { id: 3, name: "fish" },
 ];
 
 app.get("/pets", (req, res) => {
   res.send(pets);
 });
 
+app.get("/pets/:id", (req, res) => {
+  const petId = parseInt(req.params.id);
+  const pet = pets.find((pet) => pet.id === petId);
+  if (!pet) {
+    return res.status(404).send("Mascota no encontrada");
+  } else {
+    return res
+      .status(200)
+      .send({ message: "Se encontró la mascota", pet: pet });
+  }
+});
+
 app.post("/pets", (req, res) => {
-  const { body } = req;
-  mascotas.push(body);
-  res.send("se agrego mascota");
+  const newPet = {
+    id: 4,
+    name: "bird",
+  };
+  const existingPet = pets.find((pet) => pet.id === newPet.id);
+  if (existingPet) {
+    return res.status(400).send("Ya existe esa mascota");
+  }
+  pets.push(newPet);
+  res.send("Se agrego una mascota");
 });
 
 app.patch("/pets/:id", (req, res) => {
   const { body } = req;
   const { name } = body;
   const { id } = req.params;
-  let mascota = mascotas.find((mascota) => mascota.id == id);
-  mascota.name = name;
-  res.send({ message: "se elimino la mascota", mascota });
+
+  let pet = pets.find((pet) => pet.id == id);
+  if (!pet) {
+    return res.status(404).send("Mascota no encontrada");
+  }
+  pet.name = name;
+  res.send({ message: "Se actualizó la mascota", pet });
 });
 
 app.delete("/pets/:id", (req, res) => {
   const { id } = req.params;
-  mascotas = mascotas.filter((mascota) => mascota.id != id);
-  res.send({ message: "se elimino mascota" });
+  const pet = pets.find((pet) => pet.id == id);
+  if (!pet) {
+    return res.status(404).send("Mascota no encontrada");
+  }
+  pets = pets.filter((pet) => pet.id != id);
+  res.send("Se eliminó la mascota");
 });
 
-app.listen(3010);
+app.listen(3000);
